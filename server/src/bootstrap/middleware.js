8e455507e1a31619af6c028bde3d4b190aa2dae1
Eva Fridman <fridman.1999@mail.ru>
2026-08-24 12:36:55 +0300

    feat: add security headers

--- server/src/bootstrap/middleware.js
+++ server/src/bootstrap/middleware.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const cors = require('cors');
+const helmet = require('helmet');
 const cookieParser = require('cookie-parser');
 const path = require("path");
 
 const MAX_AGE = 1000 * 60 * 60 * 24 * 365;
 
 function setupMiddleware(app) {
+    app.use(helmet({
+        contentSecurityPolicy: {
+            useDefaults: true,
+            directives: { "upgrade-insecure-requests": null }, // для разработки (мешает на localhost)
+        },
+        crossOriginResourcePolicy: { policy: "cross-origin" }, // нужно до переезда клиента и api на один домен
+    }));
     app.use(cors({
         origin: 'http://localhost:5173',
         methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
@@ -13,7 +21,7 @@ function setupMiddleware(app) {
         credentials: true,
     }));
     app.use("/uploads", express.static(path.join(__dirname, "..", "uploads"), { maxAge: MAX_AGE }));
-    app.use(express.json());
+    app.use(express.json({ limit: '100kb' }));
     app.use(express.urlencoded({ extended: true }));
     app.use(cookieParser());
 }
