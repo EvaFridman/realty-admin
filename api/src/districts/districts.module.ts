444ed73e91d274e0aded3b2cec69f8a12381600d
Eva Fridman <fridman.1999@mail.ru>
2026-09-10 15:57:01 +0300

    feat: add districts adn users from db

--- api/src/districts/districts.module.ts
+++ api/src/districts/districts.module.ts
@@ -1,12 +1,10 @@
 import { Module } from '@nestjs/common';
 import { DistrictsService } from './districts.service.js';
 import { DistrictsController } from './districts.controller.js';
-import { DistrictsRepository } from './districts.repository.js';
-import { DISTRICTS_SEED } from './districts.seed.js'
 
 @Module({
   controllers: [DistrictsController],
-  providers: [DistrictsService, DistrictsRepository, { provide: "DISTRICTS_SEED", useValue: DISTRICTS_SEED } ],
+  providers: [DistrictsService],
   exports: [DistrictsService]
 })
 export class DistrictsModule {}
--- api/src/users/users.module.ts
+++ api/src/users/users.module.ts
@@ -1,12 +1,10 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service.js';
 import { UsersController } from './users.controller.js';
-import { UsersRepository } from './users.repository.js';
-import { USERS_SEED } from './users.seed.js';
 
 @Module({
   controllers: [UsersController],
-  providers: [UsersService, UsersRepository, { provide: "USERS_SEED", useValue: USERS_SEED }],
+  providers: [UsersService],
   exports: [UsersService]
 })
 export class UsersModule {}
