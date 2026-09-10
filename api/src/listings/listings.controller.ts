8903f84ede743a907b631b8bbe7a87741f7c8ed5
Eva Fridman <fridman.1999@mail.ru>
2026-09-11 01:54:24 +0300

    feat: add photos and favorites from db

--- api/src/listings/listings.controller.ts
+++ api/src/listings/listings.controller.ts
@@ -2,8 +2,9 @@ import { Controller, ParseIntPipe, Query, Body, Param, Req, Get, Post, Patch } f
 import { ListingsService } from './listings.service.js';
 import { ListListingsDto } from './dto/list-listings.dto.js';
 import { CreateListingDto } from './dto/create-listing.dto.js';
-import { UpdateListingDto } from './dto/update-listing.dto.js'
-import { UpdateStatusDto } from './dto/update-status.dto.js'
+import { UpdateListingDto } from './dto/update-listing.dto.js';
+import { UpdateStatusDto } from './dto/update-status.dto.js';
+import { UpdatePhotoDto } from './dto/update-photo.dto.js';
 import type { Request } from 'express';
 import { Roles } from '../auth/decorators/roles.decorator.js';
 
@@ -13,8 +14,8 @@ export class ListingsController {
 
     @Get()
     async findAll(@Query() query: ListListingsDto, @Req() request: Request) {
-        const currentUser = request.user as { id: number; role: string };
-        return await this.listingsService.findAll(query, currentUser);
+      const currentUser = request.user as { id: number; role: string };
+      return await this.listingsService.findAll(query, currentUser);
     }
 
     @Post()
@@ -37,5 +38,15 @@ export class ListingsController {
     @Patch(':id/status')
     async updateStatus(@Param('id', ParseIntPipe) id: number, @Body() dto: UpdateStatusDto) {
       return await this.listingsService.updateStatus(id, dto);
+    }
+
+    @Get(':id/photos')
+    async findPhotos(@Param('id', ParseIntPipe) id: number) {
+      return await this.listingsService.findPhotos(id);
+    }
+
+    @Patch(':id/photos/:photoId')
+    async updatePhoto(@Param('id', ParseIntPipe) id: number, @Param('photoId', ParseIntPipe) photoId: number, @Body() dto: UpdatePhotoDto) {
+      return await this.listingsService.updatePhoto(id, photoId, dto);
     }
 }
