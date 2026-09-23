c897b80eb2ac7039fa81e81c1b54255574fa51c4
Eva Fridman <fridman.1999@mail.ru>
2026-09-23 17:10:56 +0300

    feat: add dead letter

--- api/src/queue/topology.ts
+++ api/src/queue/topology.ts
@@ -2,10 +2,14 @@ import { Channel } from "amqplib";
 
 export async function setupTopology(channel: Channel) {
     await channel.assertExchange("portal", "topic", { durable: true });
+    await channel.assertExchange("portal.dead", "topic", { durable: true });
 
-    await channel.assertQueue("mail", { durable: true });
+    await channel.assertQueue("mail", { durable: true, arguments: { "x-dead-letter-exchange": "portal.dead", "x-dead-letter-routing-key": "mail.dead" } });
     await channel.bindQueue("mail", "portal", "viewing.#");
 
+    await channel.assertQueue("mail.dead", { durable: true });
+    await channel.bindQueue("mail.dead", "portal.dead", "mail.dead");
+    
     await channel.assertQueue("activity", { durable: true });
     await channel.bindQueue("activity", "portal", "#");
 }
