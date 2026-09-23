9ade683c14082f197c7da76ad8c1e09d35da146b
Eva Fridman <fridman.1999@mail.ru>
2026-09-23 18:14:36 +0300

    feat: add activity consumer

--- api/src/worker.module.ts
+++ api/src/worker.module.ts
@@ -5,9 +5,11 @@ import { QueueModule } from "./queue/queue.module.js";
 import { PdfService } from "./pdf/pdf.service.js";
 import { MailService } from "./mail/mail.service.js";
 import { MailConsumer } from "./queue/consumers/mail.consumer.js";
+import { ActivityConsumer } from "./queue/consumers/activity.consumer.js";
+import { RedisModule } from "./redis/redis.module.js";
 
 @Module({
-    imports: [ConfigModule.forRoot({ isGlobal: true }), PrismaModule, QueueModule],
-    providers: [PdfService, MailService, MailConsumer],
+    imports: [ConfigModule.forRoot({ isGlobal: true }), PrismaModule, QueueModule, RedisModule],
+    providers: [PdfService, MailService, MailConsumer, ActivityConsumer],
 })
 export class WorkerModule {}
